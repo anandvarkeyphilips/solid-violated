476be7d21881ba0b004684aa763db6e8f3e215f6
Krithika Balu <krithikb@thoughtworks.com>
2020-05-30 08:10:59 +0530

    Krithika - Inject dependency

--- DI - Wrong/Car.java
+++ DI - Wrong/Car.java
@@ -1,9 +1,8 @@
 public class Car {
     private final PetrolEngine engine;
 
-    public Car() {
-
-        engine = new PetrolEngine(107.2f, 20.4f, 7372.50f);
+    public Car(PetrolEngine petrolEngine) {
+        engine = petrolEngine;
     }
 
     public void start() {
--- DI - Wrong/Main.java
+++ DI - Wrong/Main.java
@@ -1,7 +1,8 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Creating a car");
-        Car car = new Car();
+        PetrolEngine petrolEngine = new PetrolEngine(107.2f, 20.4f, 7372.50f);
+        Car car = new Car(petrolEngine);
         System.out.println("Starting a car");
         car.start();
     }
